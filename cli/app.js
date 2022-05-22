a05f5f85f88aa0a619b6c65f53cbb585c8b9bf0a
Vinson Chuong <vinsonchuong@gmail.com>
2022-05-21 20:54:09 -0700

    fix(serve-ui): Ensure that CLI logs compilation errors to stdout

--- cli/app.js
+++ cli/app.js
@@ -3,7 +3,7 @@ import path from 'node:path'
 import {compose, Logger} from 'passing-notes'
 import serveUi from '../index.js'
 
-const logger = new Logger()
+export const logger = new Logger()
 
 export default compose(
   serveUi({path: path.resolve(process.env.UI_PATH), logger}),
